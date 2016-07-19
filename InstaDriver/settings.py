cbadb450eacb8c1041be4dd8b9d510b89082bb58
Olamilekan <lwahab@terragonltd.com>
2016-07-19 09:38:40 +0100

    I have committed

--- InstaDriver/settings.py
+++ InstaDriver/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'InstaDriver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'InstaDriver',
-        'USER': 'root',
-        'PASSWORD' : 'lekan',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
